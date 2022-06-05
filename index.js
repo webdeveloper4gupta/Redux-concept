7123eaf97561af0da72d3692005ed843b07cb78f
Sukritan Gupta <20051871@kiit.ac.in>
2022-06-05 16:53:59 +0530

    two reducer concept

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux=require('redux');//here i import nodemodule redux
 const createStore=redux.createStore;
 // action creater'
 const buy_cake='BUY_CAKE';  
+const buy_icecream='BUY_ICECREAM';
 function buyCake(){
     return{//action
         type:buy_cake,
@@ -9,14 +10,35 @@ function buyCake(){
     
     }   
 }
+function buyicecream(){
+    return{
+        type:buy_icecream,
+        info:'Second redux action'
+    }
+}
 
 // (previousstate,action)=>new state
 // reducer
-const initialState={
+// const initialState={
+//     numofCakes:10
+// }
+const initialCakeState={
     numofCakes:10
 }
+const initialIcecream={
+    numoficecream:20
+}
+// const reducer=(state=initialState,action)=>{
+//     switch(action.type){
+//         case buy_cake:return{
+//             ...state   ,//means making a copy of previous state
+//             numofCakes:state.numofCakes-1
+//         }
+//         default: return state
+//     }
+// }
 
-const reducer=(state=initialState,action)=>{
+const Cakereducer=(state=initialCakeState,action)=>{
     switch(action.type){
         case buy_cake:return{
             ...state   ,//means making a copy of previous state
@@ -25,10 +47,25 @@ const reducer=(state=initialState,action)=>{
         default: return state
     }
 }
+const Icecreamreducer=(state=initialIcecream,action)=>{
+    switch(action.type){
+        case buy_icecream:return{
+            ...state   ,//means making a copy of previous state
+            numoficecream:state.numoficecream-1
+        }
+        default: return state
+    }
+}
 
-const store=createStore(reducer);
+// const store=createStore(Cakereducer);//creatStore method accept only one reducer
+const combineReducer=redux.combineReducers({
+    cake:Cakereducer,
+    icecream:Icecreamreducer
+});
+const store=createStore(combineReducer)
 console.log("intitial state",store.getState())
 store.subscribe(()=>console.log('updated state',store.getState()));
 store.dispatch(buyCake());
 store.dispatch(buyCake());
-unsubscribe()//to disconnect connection 
+store.dispatch(buyicecream())
+// unsubscribe()//to disconnect connection 
